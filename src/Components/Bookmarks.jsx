06ce8cae3135f494352942faf26239bf8daa80d1
Karolin Rafalski <kr2174@caa.columbia.edu>
2023-07-27 14:43:58 -0400

    first part complete

--- src/Components/Bookmarks.jsx
+++ src/Components/Bookmarks.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bookmark from "./Bookmark";
 
+const API = import.meta.env.VITE_API_URL;
+
 function Bookmarks() {
-  const [bookmarks] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/bookmarks`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => setBookmarks(responseJSON))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="Bookmarks">
       <section>
